feefe1eeea24687c77a9705689874cc15c458797
Tobias <tobi.l@posteo.at>
2020-04-12 10:40:28 +0200

    feat: Add OffCanvas

---
+++ components/off-canvas.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import { Icon } from './icon'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+
+const OffCanvas = ({ children, open, onClose, className, direction, ...rest }) => (
+  <>
+    <div {...rest} className={classNames('off-canvas', `off-canvas--from-${direction}`, { 'off-canvas--open': open }, className)}>
+      <div name='close' className='close-icon' onClick={onClose}><Icon icon={faTimes} /></div>
+      {children}
+    </div>
+    <div className={classNames('off-canvas__click-out', { 'off-canvas__click-out--open': open })} onClick={onClose} />
+    <style jsx>{`
+      .off-canvas {
+        width: 100%;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        right: 0;
+        z-index: 200;
+        transform: translateX(100%);
+        transition: transform 0.3s ease-in-out;
+        background-color: #fff;
+        overflow-x: scroll;
+      }
+
+      @media (min-width: 700px) {
+        .off-canvas{
+          max-width: 400px;
+        }
+      }
+
+      .off-canvas--from-left {
+        transform: translateX(-100%);
+        right: unset;
+        left: 0;
+      }
+
+      .off-canvas--open {
+        transform: translateX(0);
+      }
+
+      .off-canvas__click-out {
+        visibility: hidden;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        z-index: 199;
+        background-color: #000;
+        opacity: 0;
+        cursor: pointer;
+        transition: visibility 0s, opacity 0.3s ease-in-out;
+
+      }
+      .off-canvas__click-out--open {
+        opacity: 0.25;
+        visibility: visible;
+      }
+
+      .close-icon {
+        cursor: pointer;
+        position: absolute;
+        top: 1em;
+        right: 1em;
+        z-index: 10;
+      }
+      }
+    `}
+    </style>
+  </>
+)
+
+OffCanvas.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  direction: PropTypes.oneOf(['left', 'right']),
+  onClose: PropTypes.func,
+  open: PropTypes.bool.isRequired
+}
+
+OffCanvas.defaultProps = {
+  className: '',
+  direction: 'right',
+  onClose: () => { }
+}
+
+export { OffCanvas }
--- components/top-bar.js
+++ components/top-bar.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Icon } from './icon'
 import { faBars, faFilter } from '@fortawesome/free-solid-svg-icons'
+import { OffCanvas } from './off-canvas'
 
-const TopBar = () => (
-  <>
-    <div className='top-bar'>
-      <div><Icon icon={faBars} /></div>
-      <div>Tobi's Kochbuch</div>
-      <div><Icon icon={faFilter} /></div>
-    </div>
+const TopBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [filterOpen, setFilterOpen] = useState(false)
+  return (
+    <>
+      <div className='top-bar'>
+        <div onClick={() => setMenuOpen(true)} className='action-icon'><Icon icon={faBars} /></div>
+        <div>Tobi's Kochbuch</div>
+        <div onClick={() => setFilterOpen(true)} className='action-icon'><Icon icon={faFilter} /></div>
+      </div>
+      <OffCanvas direction='left' open={menuOpen} onClose={() => setMenuOpen(false)}>
+        <p>a menu</p>
 
-    <style jsx>{`
+      </OffCanvas>
+      <OffCanvas direction='right' open={filterOpen} onClose={() => setFilterOpen(false)}>
+        <p>a filter</p>
+
+      </OffCanvas>
+
+      <style jsx>{`
       .top-bar {
         background: #fff;
         box-shadow: 0 0 25px #0005;
@@ -21,9 +33,13 @@ const TopBar = () => (
         top: 0;
         z-index: 10;
       }
+      .action-icon {
+        cursor: pointer;
+      }
     `}
-    </style>
-  </>
-)
+      </style>
+    </>
+  )
+}
 
 export { TopBar }
