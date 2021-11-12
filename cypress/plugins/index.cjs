33d94dcad7d5a17618bdab843ea169c4cb6b8b64
Tobias <tobi.l@posteo.at>
2021-11-12 19:58:50 +0100

    test: add e2e tests

---
+++ cypress/plugins/index.cjs
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef, @typescript-eslint/no-var-requires */
+/// <reference types="cypress" />
+/**
+ * @type {Cypress.PluginConfig}
+ */
+
+module.exports = function () {
+	// configure plugins here
+};
