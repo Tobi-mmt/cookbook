f639fd4d3e38adc21d8f3dc5ef1dec9dfc0db24c
Tobias <tobi.l@posteo.at>
2022-07-11 12:16:09 +0200

    test: add e2e test for subpage

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -1,6 +1,16 @@
-it('Find Bratensoße on page', function () {
+it('Navigate to recipe detail page', function () {
 	cy.visit('/');
 	cy.contains("Tobi's");
 	cy.contains('Kochbuch');
+	cy.contains('Sauerkirschbowle').scrollIntoView();
+	cy.get('a').contains('Sauerkirschbowle').click();
+	cy.contains('Sauerkirschbowle');
+	cy.contains('Sauerkirschen');
+	cy.contains('Limonade');
+});
+
+it('Jump directly to recipe detail page', function () {
+	cy.visit('/recipe/a933602f061a674/Vegane-Bratensoße', { failOnStatusCode: false });
 	cy.contains('Vegane Bratensoße');
+	cy.contains('Gemüsebrühe');
 });
