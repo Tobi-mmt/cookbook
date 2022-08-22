6468c3d815c1be265548bc452d8dfcfd5f45f6ba
Tobias <tobi.l@posteo.at>
2022-08-22 20:19:46 +0200

    chore: remove flaky test

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -8,9 +8,3 @@ it('Navigate to recipe detail page', function () {
 	cy.contains('Sauerkirschen');
 	cy.contains('Limonade');
 });
-
-it('Jump directly to recipe detail page', function () {
-	cy.visit('/recipe/a933602f061a674/Vegane-Bratensoße', { failOnStatusCode: false });
-	cy.contains('Vegane Bratensoße');
-	cy.contains('Gemüsebrühe');
-});
