293694155cc6c4ffd4ce5f1e6b6caa9925c17209
Tobias <tobi.l@posteo.at>
2023-01-23 11:22:46 +0100

    perf: add chaching

--- src/routes/api/image/[...imageName]/+server.ts
+++ src/routes/api/image/[...imageName]/+server.ts
@@ -1,6 +1,9 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import sharp from 'sharp';
 
+const ONE_DAY_IN_SECONDS = 24 * 60 * 60;
+const ONE_WEEK_IN_SECONDS = 7 * ONE_DAY_IN_SECONDS;
+
 export const GET: RequestHandler = async ({ url, params: { imageName } }) => {
 	const imageOptions = { width: 1700 };
 	if (url.searchParams.has('width')) imageOptions.width = parseInt(url.searchParams.get('width'));
@@ -15,7 +18,8 @@ export const GET: RequestHandler = async ({ url, params: { imageName } }) => {
 			headers: {
 				'access-control-allow-origin': '*',
 				'Content-Type': `image/webp`,
-				'Content-Length': image.length
+				'Content-Length': image.length,
+				'Cache-Control': `s-maxage=${ONE_DAY_IN_SECONDS}, stale-while-revalidate=${ONE_WEEK_IN_SECONDS}`
 			}
 		});
 	} catch (e: any) {
