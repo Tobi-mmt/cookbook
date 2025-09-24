34b088181f925fd82f395d0620b21c2349f03b68
Tobias <tobi.l@posteo.at>
2025-09-24 11:25:31 +0200

    refactor: replace analytics with umami.is

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,5 @@
 <script>
 	import TopBar from '$components/TopBar.svelte';
-	import { dev } from '$app/environment';
-	import { inject } from '@vercel/analytics';
-
-	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div>
